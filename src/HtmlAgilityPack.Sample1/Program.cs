3dfae3e2c466db654bae3a1a418337e0c48dc31b
eriawan <eriawan@live.com>
2023-07-19 01:19:53 +0700

    Initial creation of solution and projects

---
+++ src/HtmlAgilityPack.Sample1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace HtmlAgilityPack.Sample1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sample usage of HtmlAgilityPack v1.8.1");
+            Console.WriteLine("Sample 1: Parser sample with string and search using XPath");
+            String htmlWithFullHeader = @"<!DOCTYPE html>
+<html>
+<body>
+	<h1>This is <b>bold</b> heading</h1>
+	<p>This is <u>underlined</u> paragraph</p>
+	<h2>This is <i>italic</i> heading</h2>
+</body>
+</html> ";
+            var htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(htmlWithFullHeader);
+
+            Console.WriteLine("Before: (full HTML)");
+            Console.WriteLine(htmlWithFullHeader);
+            var htmlBody = htmlDoc.DocumentNode.SelectSingleNode("//body");
+            Console.WriteLine("After calling htmlDoc.DocumentNode.SelectSingleNode(\"//body\")");
+            Console.WriteLine(htmlBody.OuterHtml);
+        }
+    }
+}
